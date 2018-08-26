d2c734afd4faa41848794e441e894643838917df
tobatola <tobatola@gmail.com>
2018-08-25 20:52:35 -0500

    Have to add msg to commit!

--- Reactproject1.js
+++ Reactproject1.js
@@ -1,6 +1,7 @@
-//This is a test showing how I can add code to my GitHub repository from a local Git save.
-//This is a test showing how I can add code to my GitHub repository from a local Git save.
-//Now I am adding another line to see if I can get it into Github.
-//Testing a last time to see what I can get into Github.
+/*This is a test showing how I can add code to my GitHub repository from a local Git save.
+This is a test showing how I can add code to my GitHub repository from a local Git save.
+Now I am adding another line to see if I can get it into Github.
+Testing a last time to see what I can get into Github.
+NOW IM SEEING IF IVE FINALLY GOTTEN IT FIXED!*/
 
-//NOW IM SEEING IF IVE FINALLY GOTTEN IT FIXED!
+let var ilovecoding = "I love to code";
