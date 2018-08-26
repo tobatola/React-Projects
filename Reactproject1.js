e34845b88c30aedd30d7f170f83a965cb021f78c
tobatola <tobatola@gmail.com>
2018-08-25 20:27:07 -0500

    This is an update.

--- Reactproject1.js
+++ Reactproject1.js
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-This is a test of how I can add code to my GitHub repository from a local Git save.
-=======
-This is a test showing how I can add code to my GitHub repository from a local Git save.
->>>>>>> trying to connect VS Code to GitHub
-=======
 //This is a test showing how I can add code to my GitHub repository from a local Git save.
->>>>>>> starting sync
-=======
 //This is a test showing how I can add code to my GitHub repository from a local Git save.
 //Now I am adding another line to see if I can get it into Github.
->>>>>>> resetting local git
+//Testing a last time to see what I can get into Github.
