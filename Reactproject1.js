a76afcab3ffabd3e6a8f5928167758992832fbb7
tobatola <tobatola@gmail.com>
2018-08-25 20:17:52 -0500

    resetting local git

--- Reactproject1.js
+++ Reactproject1.js
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 This is a test of how I can add code to my GitHub repository from a local Git save.
 =======
 This is a test showing how I can add code to my GitHub repository from a local Git save.
@@ -7,3 +8,7 @@ This is a test showing how I can add code to my GitHub repository from a local G
 =======
 //This is a test showing how I can add code to my GitHub repository from a local Git save.
 >>>>>>> starting sync
+=======
+//This is a test showing how I can add code to my GitHub repository from a local Git save.
+//Now I am adding another line to see if I can get it into Github.
+>>>>>>> resetting local git
