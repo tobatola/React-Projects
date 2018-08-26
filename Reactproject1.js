aff29b4fb9ded955a356812159008aa259dda65e
tobatola <tobatola@gmail.com>
2018-08-25 20:07:08 -0500

    trying to connect VS Code to GitHub

--- Reactproject1.js
+++ Reactproject1.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 This is a test of how I can add code to my GitHub repository from a local Git save.
+=======
+This is a test showing how I can add code to my GitHub repository from a local Git save.
+>>>>>>> trying to connect VS Code to GitHub
