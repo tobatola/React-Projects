5c7fedb368e3c93d518062add28467630498dab3
tobatola <tobatola@gmail.com>
2018-09-12 07:29:52 -0500

    factory functions saved

--- factory functions.js
+++ factory functions.js
@@ -5,16 +5,29 @@ Factory functions can also have parameters allowing us to customize the object
 that gets returned.
 */
 
+
 /*
- monsterFactory function above, it has four parameters and returns 
- an object that has the properties: name, age, energySource, and scare(). 
+monsterFactory function above, it has four parameters and returns 
+an object that has the properties: name, age, energySource, and scare(). 
 */
 const monsterFactory = (name, age, energySource, catchPhrase) => {
     return { 
-      name: name,
-      age: age, 
-      energySource: energySource,
-      scare() {console.log(catchPhrase);} 
+      name: name,                                                     //Keyname: value
+      age: age,                                                       //Keyname: value
+      energySource: energySource,                                     //Keyname: value
+      scare() {console.log(catchPhrase);}                             //Keyname: function
+    }
+  };
+
+/*
+With ES6, it has been made easier by removing dupes when keyname was same as parameter name.
+*/
+const monsterFactory = (name, age, energySource, catchPhrase) => {    //Parameters
+    return { 
+      name,                                                           //Keyname
+      age,                                                            //Keyname
+      energySource,                                                   //Keyname
+      scare() {console.log(catchPhrase);}                             //Keyname function
     }
   };
 
@@ -25,3 +38,24 @@ return value to a variable:
 */
 const ghost = monsterFactory('Ghouly', 251, 'ectoplasm', 'BOO!');
 ghost.scare(); // 'BOO!'
+
+
+//Destructured assignment allows extract key-value pairs from objects and save them as properties.
+const robot = {
+    model: '1E78V2',
+    energyLevel: 100,
+    functionality: {
+      beep() {
+        console.log('Beep Boop');
+      },
+      fireLaser() {
+        console.log('Pew Pew');
+      },
+    }
+  };
+
+const functionality = robot.functionality       // Assigns robot.functionality key pair to function functionality
+//OR use destructured:
+const { functionality } = robot;                // Assigns robot.functionality key pair to function functionality
+functionality.beep();                           // Returns "Beep Boop"
+functionality.energyLevel;                      // Returns Error since functionality only attached to robot.functionality property
